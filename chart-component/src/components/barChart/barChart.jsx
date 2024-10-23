f71fe82bcb919b8302637e64ca43998e2050a670
Leon Gabriel <g73346920@gmail.com>
2024-10-24 00:07:00 +0300

    Initial commit

--- chart-component/src/components/barChart/barChart.jsx
+++ chart-component/src/components/barChart/barChart.jsx
@@ -1,33 +1,36 @@
-import React, { useEffect,useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 
 const chartData = () => {
-  const [data,setData] = ([]);
-  const [loading,setLoading] = (false)
+  const [data, setData] = ([]);
+  const [loading, setLoading] = (false)
 
   //Fechting bar data from data.json
   useEffect(() => {
-    async function fetchData(){
-      try{
-        const response =  await fetch('/public/data.json')
+    async function fetchData() {
+      try {
+        const response = await fetch('/public/data.json')
         const data = await response.json();
         setData(data)
-      }catch(error){
+      } catch (error) {
         throw new Error(error)
-      }finally{
+      } finally {
         setLoading(false)
       }
     }
     fetchData()
-  },[])
+  }, [])
 }
 
 
-function Bar(){
+function Bar() {
   return (
     <div className='fixed bg-primary w-[35%] h-96 ml-96 mt-5 rounded-2xl'>
       <div className='flex flex-row ml-10 mt-5 text-black text-xl font-bold  top-4'>Spending - Last 7 days</div>
-      
+      <div className='flex'>
+        <div className='bg-background w-[12%] h-12 rounded-md mt-48 ml-9'></div>
+        <p className='flex flex-row mt-60 '>mon</p>
+      </div>
     </div>
   )
 }
