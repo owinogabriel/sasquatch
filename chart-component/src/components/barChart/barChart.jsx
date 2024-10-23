9819da149f5849765f59d3d6e648ba153382eac4
Leon Gabriel <g73346920@gmail.com>
2024-10-23 15:06:59 +0300

    Initial commit

---
+++ chart-component/src/components/barChart/barChart.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+// import { Bar } from 'react-chartjs-2';
+
+
+function Bar(){
+  return (
+    <div className='fixed bg-primary w-[35%] h-96 ml-96 mt-5 rounded-2xl'>
+      <div className='flex flex-row ml-10 mt-5 text-black text-xl font-bold  top-4'>Spending - Last 7 days</div>
+      
+    </div>
+  )
+}
+
+export default Bar;
