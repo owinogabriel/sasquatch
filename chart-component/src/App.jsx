f132a0171daa40e7c2ec8d7de2f08194e2a8a7c1
Leon Gabriel <g73346920@gmail.com>
2024-10-24 16:20:46 +0300

    Initial commit

--- chart-component/src/App.jsx
+++ chart-component/src/App.jsx
@@ -1,13 +1,22 @@
 import { useState } from 'react'
 import './App.css'
 import Header from "./components/header/header";
-import Bar from './components/barChart/barChart';
+
+import BarChart from './components/barChart/barChart';
 function App() {
  
   return (
+  //   <Router className="bg-gradient-to-b from-beige-200 to-green-900 h-screen flex flex-col items-center justify-between">
+  //   <Routes>
+  //     <Route path="/" element={<Header />} />
+  //     <Route path= '/' element ={<Bar />} />
+  //     {/* <Route path="/recipe/:id" element={<RecipeDetail />} />
+  //     <Route path="/recipe/:id" element={<AddRecipeForm />} /> */}
+  //   </Routes>
+  // </Router>
     <>
     <Header />
-    <Bar />
+    <BarChart />
      </>
   )
 }
