ef7a264e96a332841bc9c8f9b999abeeac31fc3c
Leon Gabriel <g73346920@gmail.com>
2024-10-25 20:36:37 +0300

    Modified

--- chart-component/src/App.jsx
+++ chart-component/src/App.jsx
@@ -4,20 +4,14 @@ import Header from "./components/header/header";
 
 import BarChart from './components/barChart/barChart';
 function App() {
- 
+
   return (
-  //   <Router className="bg-gradient-to-b from-beige-200 to-green-900 h-screen flex flex-col items-center justify-between">
-  //   <Routes>
-  //     <Route path="/" element={<Header />} />
-  //     <Route path= '/' element ={<Bar />} />
-  //     {/* <Route path="/recipe/:id" element={<RecipeDetail />} />
-  //     <Route path="/recipe/:id" element={<AddRecipeForm />} /> */}
-  //   </Routes>
-  // </Router>
+
     <>
-    <Header />
-    <BarChart />
-     </>
+      <Header />
+      <BarChart />
+    </>
+    
   )
 }
 
