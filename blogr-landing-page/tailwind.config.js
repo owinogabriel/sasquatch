bf873ec595fb5f768866e81237f151203a54f30f
Leon Gabriel <g73346920@gmail.com>
2024-10-31 23:54:53 +0300

    modified

--- blogr-landing-page/tailwind.config.js
+++ blogr-landing-page/tailwind.config.js
@@ -2,7 +2,10 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: 'media', 
   theme: {
-    extend: {},
+    extend: { backgroundImage: {
+      'hero-pattern': "url('/assets/bg.svg')",
+      'footer-texture': "url('/img/footer-texture.png')",
+    }},
   },
   plugins: [],
 };
