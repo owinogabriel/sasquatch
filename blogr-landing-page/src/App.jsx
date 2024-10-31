71a5d62a9ac0c49da6fb67996b8343561e6fe96b
Leon Gabriel <g73346920@gmail.com>
2024-11-01 01:40:21 +0300

    Chored:Import Context from the landing page.

--- blogr-landing-page/src/App.jsx
+++ blogr-landing-page/src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 
-import NavBar from './components/Navbar/Navbar'
+import NavBar from './components/landingPage/Navbar'
+import Content from './components/landingPage/content'
 
 function App() {
 
   return (
     <>
     <NavBar />
+    <Content />
     </>
   )
 }
