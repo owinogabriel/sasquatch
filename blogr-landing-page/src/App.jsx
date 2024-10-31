8d7071fee59e4a45df33910d1b534d36d79b2351
Leon Gabriel <g73346920@gmail.com>
2024-10-31 16:17:16 +0300

    Modified

--- blogr-landing-page/src/App.jsx
+++ blogr-landing-page/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
-import Header from './components/Header/header'
+
+import NavBar from './components/Navbar/Navbar'
 
 function App() {
 
   return (
     <>
-    <Header />
+    <NavBar />
     </>
   )
 }
