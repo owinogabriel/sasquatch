05f4671a6027194308e77bcf6f81293059358d67
Leon Gabriel <g73346920@gmail.com>
2024-11-02 01:35:48 +0300

    Chored:Last Commit.

--- blogr-landing-page/src/components/designed/future.jsx
+++ blogr-landing-page/src/components/designed/future.jsx
@@ -2,27 +2,32 @@ import React from 'react'
 
 const Future = () => {
   return (
-    <div>
+    <div className='flex mt-40 py-16 px-10 text-justify bg-no-repeat bg-right' style={{
+      backgroundImage: "url('assets/editor-desktop.svg')",
+      backgroundSize: '46%',
+
+
+    }}>
       <div>
 
-        <div>
+        <div className=' text-center ml-[70%] font-sans font-bold w-max -mt-10 mb-28 color-[#405973] text-3xl' style={{ color: '#405973' }}>
           <h1>Designed for the future</h1>
         </div>
 
-        <div>
-          <h2>Introduction and extensible editor</h2>
-          <p>Blogr feature an exceedingly intuitive interface which lets you focus <br />
+        <div className='mb-10'>
+          <h2 className='mb-2 font-sans font-bold w-max -mt-14  text-xl' style={{ color: '#405973' }}>Introduction and extensible editor</h2>
+          <p className='font-normal mb-12' style={{ color: '#AFB3B6' }}>Blogr feature an exceedingly intuitive interface which lets you focus <br />
             on one thin: creating content. The editor supports management of <br /> multiples blogs and allows easy manipulation of embeds such as images, <br />
             videos, and Markdown. Extensibility with plugins and themes provide <br /> easy ways to add
             functoinality or change the looks blog.</p>
         </div>
 
-        <div>
-          <h2>Robust content management</h2>
-          <p>Flexible content management enables users to easily move through <br /> posts. Increase the usability of your blog by adding customized <br />
+        <div className='mt-24'>
+          <h2 className='mb-2 font-sans font-bold w-max -mt-14  text-xl' style={{ color: '#405973' }}>Robust content management</h2>
+          <p className='font-normal' style={{ color: '#AFB3B6' }}>Flexible content management enables users to easily move through <br /> posts. Increase the usability of your blog by adding customized <br />
             categories, section, format, or flow. With this functionality, you're in <br /> full control.</p>
         </div>
-        
+
       </div>
     </div>
   )
