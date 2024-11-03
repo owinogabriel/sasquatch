abe04ff8b003cb6768b4ea4421f3b89e618be4ce
Leon Gabriel <g73346920@gmail.com>
2024-11-03 16:18:58 +0300

    Initial commit

---
+++ blogr-landing-page/src/components/Blogr/blogr.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+export const Blogr = () => {
+  return (
+    <div className='flex  w-screen  bg-[#373854] h-[50vh] bg-cover text-justify
+   -ml-10 rounded-bl-[50px]  bg-left-bottom 
+     rounded-tr-[60px] mt-16 bg-no-repeat' style={{
+        backgroundImage: "url('assets/bg-circles.svg')",
+        backgroundSize: '50%',
+      }}>
+
+      <div className='ml-36'>
+        <img src="./assets/illustration-phones.svg" alt="" className='w-[65%]' />
+      </div>
+
+      <div className=' inline-block'>
+
+        <h2 className='  font-sans
+         font-bold mt-[3rem] -ml-[8.1rem] 
+        color-[#405973] text-2xl'
+          style={{ color: '#FFFFFF' }}>
+          State of the Art Infrastructure
+        </h2>
+
+        <p className='font-sm mt-5 -ml-32 mr-56 text-stone-100'>With reliability and speed in mind.worldwide data centers provide <br />
+          backbone for ultra-fast conectivity. This ensures your site will load <br />
+          instantly, no matter where you readers are, keeping your site <br />
+          competitive.</p>
+      </div>
+
+    </div>
+  )
+}
+export default Blogr;
