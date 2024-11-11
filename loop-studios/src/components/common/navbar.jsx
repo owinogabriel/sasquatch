22e50be22f02cd67b50151868ded6d69e9f40e73
Leon Gabriel <g73346920@gmail.com>
2024-11-11 21:28:17 +0300

    chored:bg-image set to no repeat.

---
+++ loop-studios/src/components/common/navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className="w-screen h-[100vh] justify-between bg-[url('/images/desktop/hero.jpg')] md:max-xl:flex flex text-white bg-no-repeat">
+      <div className='mt-[4rem] ml-[10rem] text-4xl font-extrabold'>loopstudios</div>
+      <div className='mr-[5rem] mt-[5rem] '>
+        <ul className='inline-flex gap-6'>
+          <li><a href="">About</a></li>
+          <li><a href="">Careers</a></li> 
+          <li><a href="">Events</a></li>
+          <li><a href="">Products</a></li>
+          <li><a href="">Support</a></li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+ 
