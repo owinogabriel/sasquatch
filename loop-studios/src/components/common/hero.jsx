4efc3c37b51c78ad5e1ceb74f1917ccf26f7bb3e
Leon Gabriel <g73346920@gmail.com>
2024-11-11 22:38:29 +0300

    Fisrt commit

---
+++ loop-studios/src/components/common/hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <div className='mt-36'>
+      <div className='mt-36 text-fuchsia-500'>
+        Hello
+      </div>
+    </div>
+  )
+}
