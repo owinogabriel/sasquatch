00554e519665339070d78474047970c1ec6bfc5e
Leon Gabriel <g73346920@gmail.com>
2024-11-11 23:40:08 +0300

    chored:modified

--- loop-studios/src/App.jsx
+++ loop-studios/src/App.jsx
@@ -1,17 +1,17 @@
 import { useState } from 'react'
-import Navbar from './components/common/navbar'
+import Navbar from './components/common/navbar/navbar'
 import Hero from './components/common/hero'
 
 
 
 function App() {
- 
+
 
   return (
-   <>
-   <Navbar />
-<Hero />
-   </>
+    <>
+      <Navbar />
+      <Hero />
+    </>
   )
 }
 
