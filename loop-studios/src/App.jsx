f14171c5e234bdf0f3141c684b9eca127852793b
Leon Gabriel <g73346920@gmail.com>
2024-11-12 11:53:34 +0300

    chored:import hero from the components

--- loop-studios/src/App.jsx
+++ loop-studios/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import Navbar from './components/common/navbar/navbar'
-import Hero from './components/common/hero'
+import Hero from './components/common/hero/hero'
 
 
 
