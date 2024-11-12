ba05e645e377cc8c336d578680979a05231813c2
Leon Gabriel <g73346920@gmail.com>
2024-11-12 12:28:19 +0300

    hored:bg-image size reduced.

--- loop-studios/src/components/common/hero/hero.jsx
+++ loop-studios/src/components/common/hero/hero.jsx
@@ -1,18 +1,26 @@
-import React from 'react'
+import React from 'react';
 
-export default Hero = () => {
+const Hero = () => {
   return (
-    <div className="ml-[20]rem] bg-contain bg-center bg-[url('/images/desktop/interactive.jpg')] bg-no-repeat flex ml-[25rem]  mt-[10rem] mb-[5rem]" style={{ backgroundSize: '' }}>
-
-      <div className="bg-white ">
-        <div className='bg-white bg-contain'>
-          <h1 className='text-3xl mb-8'>THE LEADER IN <br /> INTERACTIVE VR</h1>
-          <p>Founded in2011, Loopstudios has been producing worl-class <br />virtual reality projects for some of the best companies around the <br />globe. Our award-winning creations have transformed <br /> businesses through digital experiences that blind to their brand.</p>
-        </div>
-
+    <div
+      className="flex flex-col bg-[url('/images/desktop/hrr.jpg')] md:flex-row items-center justify-center mt-10 mb-5 ml-[rem] bg-no-repeat bg-center"
+      style={{
+        backgroundSize: '50%',
+      }}
+    >
+      <div className="bg-white p-6 md:p-10 max-w-md ml-[48rem] mt-[20rem] ">
+        <h1 className="text-3xl font-bold mb-4">
+          THE LEADER IN <br /> INTERACTIVE VR
+        </h1>
+        <p className="text-gray-600">
+          Founded in 2011, Loopstudios has been producing world-class <br />
+          virtual reality projects for some of the best companies around the <br />
+          globe. Our award-winning creations have transformed <br />
+          businesses through digital experiences that bind to their brand.
+        </p>
       </div>
     </div>
+  );
+};
 
-
-  )
-}
+export default Hero;
