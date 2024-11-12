8b4e10203d73cbd1a40ebee718a3ee9440f13a4f
Leon Gabriel <g73346920@gmail.com>
2024-11-12 11:52:36 +0300

    Modified

--- loop-studios/tailwind.config.js
+++ loop-studios/tailwind.config.js
@@ -1,12 +1,17 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: 'media', 
   theme: {
-    extend: {},
+    extend: {   backgroundSize: {
+      '40%': '40%',
+      '120%': '120%',
+    },},
+
+    screens:{
+      'sm': '640px'
+    }
   },
   plugins: [],
-}
+};
 
 
