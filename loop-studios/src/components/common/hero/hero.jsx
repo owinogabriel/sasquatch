6720d7acf5f190479b8d83c04de552be8c44e053
Leon Gabriel <g73346920@gmail.com>
2024-11-12 12:00:49 +0300

    chored:document formatted well

--- loop-studios/src/components/common/hero/hero.jsx
+++ loop-studios/src/components/common/hero/hero.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 
-export default function Hero() {
+export default Hero = () => {
   return (
-    <div className="ml-[20]rem] bg-contain bg-center bg-[url('/images/desktop/interactive.jpg')] bg-no-repeat flex ml-[25rem]  mt-[10rem] mb-[5rem]" style={{backgroundSize:''}}>
-    
-         <div className="bg-white ">
-          <div className='bg-white bg-contain'>
+    <div className="ml-[20]rem] bg-contain bg-center bg-[url('/images/desktop/interactive.jpg')] bg-no-repeat flex ml-[25rem]  mt-[10rem] mb-[5rem]" style={{ backgroundSize: '' }}>
+
+      <div className="bg-white ">
+        <div className='bg-white bg-contain'>
           <h1 className='text-3xl mb-8'>THE LEADER IN <br /> INTERACTIVE VR</h1>
-        <p>Founded in2011, Loopstudios has been producing worl-class <br />virtual reality projects for some of the best companies around the <br />globe. Our award-winning creations have transformed <br /> businesses through digital experiences that blind to their brand.</p>
-          </div>
-        
+          <p>Founded in2011, Loopstudios has been producing worl-class <br />virtual reality projects for some of the best companies around the <br />globe. Our award-winning creations have transformed <br /> businesses through digital experiences that blind to their brand.</p>
+        </div>
+
       </div>
-      </div>
-     
-    
+    </div>
+
+
   )
 }
