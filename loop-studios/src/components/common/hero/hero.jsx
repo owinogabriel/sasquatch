7279cd382b59335328f7c9c38698ff7f184de06d
Leon Gabriel <g73346920@gmail.com>
2024-11-13 11:50:46 +0300

    chored:Modified

--- loop-studios/src/components/common/hero/hero.jsx
+++ loop-studios/src/components/common/hero/hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
         backgroundSize: '50%',
       }}
     >
-      <div className="bg-white p-6 md:p-10 max-w-md ml-[48rem] mt-[20rem] ">
+      <div className="bg-white p-6 md:p-10 max-w-md ml-[48rem] mt-[20rem] md:grid-cols-2 items-center justify-center ">
         <h1 className="text-3xl font-bold mb-4">
           THE LEADER IN <br /> INTERACTIVE VR
         </h1>
