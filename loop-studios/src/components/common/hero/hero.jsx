dc72b95a7cb3f9022662960433350feb274b852e
Leon Gabriel <g73346920@gmail.com>
2024-11-13 13:36:10 +0300

    Initial commit

--- loop-studios/src/components/common/hero/hero.jsx
+++ loop-studios/src/components/common/hero/hero.jsx
@@ -2,24 +2,26 @@ import React from 'react';
 
 const Hero = () => {
   return (
-    <div
-      className="flex flex-col bg-[url('/images/desktop/hrr.jpg')] md:flex-row items-center justify-center mt-10 mb-5 ml-[rem] bg-no-repeat bg-center"
-      style={{
-        backgroundSize: '50%',
-      }}
-    >
-      <div className="bg-white p-6 md:p-10 max-w-md ml-[48rem] mt-[20rem] md:grid-cols-2 items-center justify-center ">
-        <h1 className="text-3xl font-bold mb-4">
-          THE LEADER IN <br /> INTERACTIVE VR
-        </h1>
-        <p className="text-gray-600">
-          Founded in 2011, Loopstudios has been producing world-class <br />
-          virtual reality projects for some of the best companies around the <br />
-          globe. Our award-winning creations have transformed <br />
-          businesses through digital experiences that bind to their brand.
-        </p>
+    <section className="mt-10 mb-5 px-4 md:px-20">
+      <div className="flex flex-col md:flex-row items-center justify-center md:justify-start">
+        <img
+          src="/images/desktop/hrr.jpg"
+          alt="Person using VR headset"
+          className="w-full md:w-1/2 object-scale-down"
+        />
+        <div className="bg-white p-6 md:p-10 max-w-md md:ml-0 md:mt-0 text-center md:text-left  -mt-[20rem] ml-[40rem] goat">
+          <h1 className="text-2xl font-bold mb-4 ">
+            THE LEADER IN <br /> INTERACTIVE VR
+          </h1>
+          <p className="text-gray-600 text-start text-lg">
+            Founded in 2011, Loopstudios has been producing world-class
+            virtual reality projects for some of the best companies around the
+            globe. Our award-winning creations have transformed
+            businesses through digital experiences that bind to their brand.
+          </p>
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
 
