cc51ced7517273d113d62eaa6f558304153de7a2
Leon Gabriel <g73346920@gmail.com>
2024-11-13 23:48:53 +0300

    chored:Mobile Layut well designed

--- loop-studios/src/components/creations/creation.jsx
+++ loop-studios/src/components/creations/creation.jsx
@@ -12,22 +12,33 @@ const Creation = () => {
     { src: '/images/desktop/fisheye.jpg', text: ' MAKE IT FISHEYE' }
   ];
 
+  const mobile = [
+    { src: '/images/mobile/deep.jpg', text: 'DEEP EARTH' },
+    { src: '/images/mobile/night.jpg', text: 'NIGHT ARCADE' },
+    { src: '/images/mobile/soccer.jpg', text: 'SOCCER TEAM VR' },
+    { src: '/images/mobile/grid.jpg', text: ' THE GRID' },
+    { src: '/images/mobile/above.jpg', text: 'FROM UP ABOVE VR' },
+    { src: '/images/mobile/pocket.jpg', text: ' POCKET BOREALIS' },
+    { src: '/images/mobile/curiosity.jpg', text: 'THE CURIOSITY' },
+    { src: '/images/mobile/fisheye.jpg', text: 'MAKE IT FESHEYE' },
+  ]
+
   return (
-    <section className='mt-[4rem] mb-[4rem]'>
+    <section className='mt-[4rem] mb-[4rem] '>
 
       {/* {creation and button } */}
       <div className='flex justify-between'>
-        <div className='text-3xl font-light ml-[4rem] mb-[2rem]'>
+        <div className='text-3xl font-light ml-[4rem] mb-[2rem] header'>
           <h1>OUR CREATIONS</h1>
         </div>
 
-        <div className='mr-[5rem] font-normal text-base'>
-          <button className='border border-black px-4 rounded-sm'>SEE ALL</button>
+        <div className='mr-[5rem] font-normal text-base gallery'>
+          <button className='border border-black px-4 rounded-sm md:hidden'>SEE ALL</button>
         </div>
 
       </div>
-
-      <div className="grid grid-cols-4 gap-4 p-4 md:grid-cols-2 lg:grid-cols-4   ml-[3rem] mr-[3rem]">
+      {/* {desktop gallery} */}
+      <div className="grid grid-cols-4 gap-4 p-4 md:grid-cols-2 lg:grid-cols-4   ml-[3rem] mr-[3rem] gallery">
         {items.map((item, index) => (
           <div key={index} className="relative rounded overflow-hidden">
             <img src={item.src} alt={`Image ${index + 1}`} className="w-full h-auto cursor-pointer hover:bg-sky-500" />
@@ -37,6 +48,25 @@ const Creation = () => {
             </div>
           </div>
         ))}
+
+      </div>
+
+      {/*mobile layout*/}
+      <div className="grid grid-cols-1 gap-4 p-4 md:grid-cols-2 lg:grid-cols-4   ml-[3rem] mr-[3rem] mobile">
+        {mobile.map((item, index) => (
+          <div key={index} className="relative rounded overflow-hidden">
+            <img src={item.src} alt={`Image ${index + 1}`} className="w-full h-auto cursor-pointer hover:bg-sky-500" />
+            {/* Text overlay */}
+            <div className="absolute inset-0 flex items-center justify-center text-lg mt-[4rem] mr-[5rem] text-start">
+              <span className="text-white  font-light">{item.text}</span>
+            </div>
+
+          </div>
+
+        ))}
+        <div className='ml-[12rem] font-normal text-base mobile'>
+          <button className='border border-black px-4 rounded-sm md:hidden bnt'>SEE ALL</button>
+        </div>
       </div>
     </section>
   );
