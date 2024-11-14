fcbff23f8cd71c7eaefc3dac0352940b23243636
Leon Gabriel <g73346920@gmail.com>
2024-11-14 22:48:21 +0300

    Chored:Last commit.

--- loop-studios/src/components/common/footer/Footer.jsx
+++ loop-studios/src/components/common/footer/Footer.jsx
@@ -19,11 +19,34 @@ export const Footer = () => {
 
         <div className='text-white mt-[3rem]  hidden list2'>
           <ul className=' space-x-3 gap-6 text-justify -ml-[8rem] mt-5 grid grid-cols-1 '>
-          <a href="#" className="hover:underline ml-3">About</a>
+            <a href="#" className="hover:underline ml-3">About</a>
             <a href="#" className="hover:underline ml-3">Careers</a>
             <a href="#" className="hover:underline">Events</a>
             <a href="#" className="hover:underline">Products</a>
             <a href="#" className="hover:underline">Support</a></ul>
+          <div className='inline-flex justify-between gap-4 -ml-[10rem]  mt-[2rem]'>
+
+            <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer ">
+              <img src="images/facebook.svg" alt="Facebook" />
+            </a>
+
+            <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">
+              <img src="images/twitter.svg" alt="Facebook" />
+            </a>
+
+            <a href="https://www.pinterest.com" target="_blank" rel="noopener noreferrer">
+              <img src="images/pinterest.svg" alt="Facebook" />
+            </a>
+
+            <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+              <img src="images//instagram.svg" alt="Facebook" />
+            </a>
+            <footer className='text-[#646363] -ml-[14rem] text-base mt-[2rem]'>
+            &copy;2021 Loopstudios.All rights reserved.
+          </footer>
+          </div>
+       
+
         </div>
         <div className=' ml-[30rem]'>
 
