ae3f02c21f3e7752c48ab452b39215e37bf7af5b
Leon Gabriel <g73346920@gmail.com>
2024-11-28 12:41:03 +0300

    chored:item align cnter on md screens.

--- loop-studios/src/components/common/hero/hero.jsx
+++ loop-studios/src/components/common/hero/hero.jsx
@@ -2,19 +2,19 @@ import React from 'react';
 
 const Hero = () => {
   return (
-    <section className="max-w-full flex md:flex-row flex-col items-start justify-start md:space-y-10">
-        <div className="h-60 w-full md:h-96 md:w-96 flex items-center jusitify-center">
+    <section className="max-w-full flex md:flex-row flex-col items-start justify-center md:space-y-10">
+        <div className="h-60 w-full md:h-96 md:w-96  flex items-center jusitify-center">
         <img
           src="/images/desktop/hrr.jpg"
           alt="Person using VR headset"
           className="object-contain w-full h-[100%]"
         />
         </div>
-        <div className="p-10 flex flex-col items-center justify-center w-full">
-          <h1 className="md:text-6xl text-4xl font-regular mb-4  md:w-full w-96 md:text-start text-center">
+        <div className="p-10  flex flex-col items-center justify-center w-full md:w-[30rem] md:text-start">
+          <h1 className="md:text-3xl text-4xl font-regular mb-4  md:w-[15rem] w-96 md:text-start text-center">
             THE LEADER IN INTERACTIVE VR
           </h1>
-          <p className="w-[80%] text-gray-600 md:text-start text-lg md:text-start text-center">
+          <p className="w-[80%] text-gray-600 bg-white md:text-start text-lg text-center">
             Founded in 2011, Loopstudios has been producing world-class
             virtual reality projects for some of the best companies around the
             globe. Our award-winning creations have transformed
