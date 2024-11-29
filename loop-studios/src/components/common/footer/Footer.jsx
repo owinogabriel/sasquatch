b6b2b8e8c6bdfca8a55f5e61fa81da994fdc7b11
Leon Gabriel <g73346920@gmail.com>
2024-11-29 15:43:09 +0300

    chored:Footer modified

--- loop-studios/src/components/common/footer/Footer.jsx
+++ loop-studios/src/components/common/footer/Footer.jsx
@@ -1,82 +1,87 @@
-import React from 'react'
+import React from "react";
 
 export const Footer = () => {
   return (
+    <section className="bg-black py-6">
+      {/* Wrapper for all content */}
+      <div className="max-w-7xl mx-auto px-6 lg:px-12 flex flex-col md:flex-row justify-between items-center">
+        {/* Left Section: Logo and Navigation */}
+        <div className="text-white">
+          {/* Logo */}
+          <h1 className="text-3xl font-bold">loopstudios</h1>
 
-    <section className='bg-black w-full h-full flex mb-0 footer'>
-      <div className='flex md:mt-[2rem] md:ml-[8rem]'>
-
-        <div className='text-white text-3xl font-bold logo2'>
-          <h1>loopstudios</h1>
+          {/* Navigation Links */}
+          <nav className="mt-4">
+            <ul className="flex flex-wrap gap-4 md:gap-8 text-sm">
+              <li>
+                <a href="#" className="hover:underline">
+                  About
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:underline">
+                  Careers
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:underline">
+                  Events
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:underline">
+                  Products
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:underline">
+                  Support
+                </a>
+              </li>
+            </ul>
+          </nav>
         </div>
-        <div className='text-white mt-[3rem] list'>
-          <ul className=' space-x-3 -ml-[10.5rem] md:grid-cols-2 lg:grid-cols-1'>
-            <a href="#" className="hover:underline">Careers</a>
-            <a href="#" className="hover:underline">Events</a>
-            <a href="#" className="hover:underline">Products</a>
-            <a href="#" className="hover:underline">Support</a></ul>
-        </div>
 
-        <div className='text-white mt-[3rem] ml-[1rem] hidden list2'>
-          <ul className=' space-x-3 gap-6 text-justify -ml-[8rem] mt-5 grid grid-cols-1 '>
-            <a href="#" className="hover:underline ml-3">About</a>
-            <a href="#" className="hover:underline ml-3">Careers</a>
-            <a href="#" className="hover:underline">Events</a>
-            <a href="#" className="hover:underline">Products</a>
-            <a href="#" className="hover:underline">Support</a></ul>
-          <div className='inline-flex justify-between gap-4 -ml-[10rem]  mt-[2rem]'>
-
-            <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer ">
-              <img src="images/facebook.svg" alt="Facebook" />
+        {/* Right Section: Social Media and Copyright */}
+        <div className="mt-8 md:mt-0 text-white text-center md:text-right">
+          {/* Social Media Icons */}
+          <div className="flex justify-center md:justify-end gap-4">
+            <a
+              href="https://www.facebook.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src="images/facebook.svg" alt="Facebook" className="w-6" />
             </a>
-
-            <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">
-              <img src="images/twitter.svg" alt="Facebook" />
+            <a
+              href="https://www.twitter.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src="images/twitter.svg" alt="Twitter" className="w-6" />
             </a>
-
-            <a href="https://www.pinterest.com" target="_blank" rel="noopener noreferrer">
-              <img src="images/pinterest.svg" alt="Facebook" />
+            <a
+              href="https://www.pinterest.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src="images/pinterest.svg" alt="Pinterest" className="w-6" />
             </a>
-
-            <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
-              <img src="images//instagram.svg" alt="Facebook" />
+            <a
+              href="https://www.instagram.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src="images/instagram.svg" alt="Instagram" className="w-6" />
             </a>
-            <footer className='text-[#646363] mb-[1rem] -ml-[13.8rem] text-base mt-[2rem]'>
-              &copy;2021 Loopstudios.All rights reserved.
-            </footer>
           </div>
 
-
-        </div>
-        <div className=' ml-[3rem] mtn'>
-
-          <div className='inline-flex justify-between gap-4 ml-[8rem] '>
-
-            <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer ">
-              <img src="images/facebook.svg" alt="Facebook" />
-            </a>
-
-            <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">
-              <img src="images/twitter.svg" alt="Facebook" />
-            </a>
-
-            <a href="https://www.pinterest.com" target="_blank" rel="noopener noreferrer">
-              <img src="images/pinterest.svg" alt="Facebook" />
-            </a>
-
-            <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
-              <img src="images//instagram.svg" alt="Facebook" />
-            </a>
-          </div>
-          <footer className='text-[#646363] mr-[5rem] text-base mt-[1rem] hrm'>
-            &copy;2021 Loopstudios.All rights reserved.
+          {/* Copyright */}
+          <footer className="text-gray-500 text-sm mt-4">
+            &copy; 2021 Loopstudios. All rights reserved.
           </footer>
-
         </div>
-
       </div>
-
     </section>
-
-  )
-}
+  );
+};
