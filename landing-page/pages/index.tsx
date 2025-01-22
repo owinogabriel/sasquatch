faa94e322b2d7ccac65547dfac0e2372fb05a74e
Leon Gabriel <g73346920@gmail.com>
2025-01-22 23:46:02 +0300

    initial commit.

--- landing-page/pages/index.tsx
+++ landing-page/pages/index.tsx
@@ -21,10 +21,10 @@ export default function Home() {
     >
       <main className="flex gap-8 row-start-2 items-center sm:items-start">
 
-       {/* Hero section */}
-        <div className="gap-4 ml-[9rem] items-center align-text-top mt-[3rem]">
-          <h1 className="md:text-6xl text-[#242E52] font-bold">Bring everyone <br /> together to build <br /> better products.</h1>
-          <p className="text-[#3d3e3f] py-6">Manage makes it simple for software teams <br /> to plan day-to-day tasks while keeping the <br />
+        {/* Hero section */}
+        <div className="gap-4 md:ml-[9rem] items-center align-text-top md:mt-[3rem]">
+          <h1 className="md:text-5xl text-[#242E52] font-bold">Bring everyone <br /> together to build <br /> better products.</h1>
+          <p className="text-[#bcb9b9] py-4 font-extralight text-sm">Manage makes it simple for software teams <br /> to plan day-to-day tasks while keeping the <br />
             larger team goals in view.</p>
 
           <Button title="Get Started" /> // Button imported from the reusable button components.
@@ -39,9 +39,35 @@ export default function Home() {
             />
           </div>
 
+          <div className="md:mt-[6rem] md:flex-1">
+            <h1 className="md:text-3xl text-[#242E52] font-bold">What's different about <br /> Manage?</h1>
+            <p className="text-[#bcb9b9] py-4 font-extralight text-sm">Manage provides all the functionality your <br />
+              teams needs, without the complexity. Our <br />
+              software is tailor-made for modern digital <br />
+              products teams.</p>
+
+            <div className="absolute md:bottom-2 md:right-[2rem]">
+              <div className="inline-flex text-lg items-center justify-center gap-4 text-[#242E52] font-bold rounded-full">
+                <span className="h-10 w-10 py-2 px-2  bg-[#FF6B6B] text-white font-bold rounded-full">02</span>
+
+                Advanced built-in reports
+
+              </div>
+              <div>
+                {/* <h3 className="text-lg font-bold text-[#242E52]">
+                  Advanced built-in reports
+                </h3> */}
+                <p className="mt-2 text-[#bcb9b9] py-4 font-extralight text-sm">
+                  Set internal delivery estimates and track progress toward <br /> company goals. Our customizable dashboard helps youb <br /> build out the reports you need to keep key stakeholders <br /> informed.
+                </p>
+              </div>
+            </div>
+            <div>
 
 
+            </div>
 
+          </div>
         </div>
 
       </main>
